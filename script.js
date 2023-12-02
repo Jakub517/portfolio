53a9a888ba5c79672e0d067561975b19907cfcd9
Jakub517 <pavlu.jakub8@seznam.cz>
2023-12-02 10:24:03 +0100

    Nový design portofolia, Vánoční atmosféra.

---
+++ script.js
@@ -0,0 +1,47 @@
+
+      function createSnowflake() {
+    
+        var snowflake = $('<div class="snowflake">❄️</div>').css({
+            top: Math.random() * $(window).height(),
+            left: Math.random() * $(window).width(),
+        });
+
+      
+        $('body').append(snowflake);
+
+      
+        snowflake.animate(
+            {
+                top: $(window).height(),
+                left: '+=' + (Math.random() * 200 - 100),
+                opacity: 0,
+            },
+            Math.random() * 5000 + 5000, 
+            function () {
+              
+                $(this).remove();
+            
+                createSnowflake();
+            }
+        );
+    }
+
+
+    $(document).ready(function () {
+        for (var i = 0; i < 20; i++) {
+            createSnowflake();
+        }
+    });
+
+
+$(document).ready(function() {
+
+    $('.scroll-link').click(function(event) {
+      event.preventDefault();
+      $('html, body').animate(
+        { scrollTop: $($(this).attr('href')).offset().top },
+        800
+      );
+    });
+  });
+  
